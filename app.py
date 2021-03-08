e3978a17f4088bc1b1603fb9332bf6dd0d10aee5
unknown <jazzlynphan96@gmail.com>
2021-03-08 22:15:29 +1100

    final

--- app.py
+++ app.py
@@ -41,8 +41,23 @@ def home():
             f"/api/v1.0/<start><br><br>"
             f"/api/v1.0/<start>/<end>")
 
+# gettin the precipitation
 
-# 4. Define what to do when a user hits the /about route
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    year_ago = dt.datetime(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).filter(
+        Measurement.date >= year_ago).all()
+    session.close()
+    prcp_dict = {date: prcp for date, prcp in precipitation}
+
+    return jsonify(prcp_dict)
+
+# getting the stations
+
+
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
@@ -50,6 +65,44 @@ def stations():
     stations_list = list(np.ravel(stations))
     session.close()
     return jsonify(stations_list)
+
+# getting the temperature for previous year
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    tobs = session.query(Measurement.tobs).filter(
+        Measurement.station == 'USC00519281').filter(Measurement.date >= year_ago).all()
+    session.close()
+    tobs_list = list(np.ravel(tobs))
+
+    return jsonify(tobs_list)
+
+# getting minimum temperature, avg temperature and max temperature when give date
+
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    session = Session(engine)
+    temperature = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    session.close()
+    temp_list = list(np.ravel(temperature))
+
+    return jsonify(temp_list)
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start, end):
+    session = Session(engine)
+    temperature = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    session.close()
+    temp_list = list(np.ravel(temperature))
+
+    return jsonify(temp_list)
 
 
 if __name__ == "__main__":
